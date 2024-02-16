f3d17b405c7938bc6a5eedd8a7e0892240d34638
Archana Kumari <archanakumari123.co@gmail.com>
2024-02-16 16:10:21 +0530

    "finished the banner ui"

--- src/ui-components/HomeParts/Banner.jsx
+++ src/ui-components/HomeParts/Banner.jsx
@@ -1,10 +1,12 @@
 import Button from "../NavbarIcon/Button";
+import DocsBtn from "./DocsBtn";
+import ForYou from "./ForyouBtn";
 import SearchBar from "./SearchBar";
 
 const Banner = () => {
   return (
     <>
-    <div className="w-full h-60 mt-5 relative bg-gradient-to-r from-blue-500 to-purple-500 mx-8 rounded-md">
+    <div className=" relative w-full h-60 mt-5 relative bg-gradient-to-r from-blue-500 to-purple-500 mx-8 rounded-md">
         <div className="">
         <h1 className="text-white font-bold text-3xl text-center mt-5">What will you design today ?</h1>
         <div className=" absolute top-3 right-3">
@@ -12,6 +14,11 @@ const Banner = () => {
         <Button name="Upload" style=" bg-white bg-opacity-10"/>
          </div>
      <SearchBar/>
+        </div>
+        <div className=" absolute bottom-0 left-9" style={{width:"0px",height:"0px",borderBottom:"9px solid white",borderLeft:"8px solid transparent",borderRight:"8px solid transparent"}}></div>
+        <div className="mt-7 ml-5 flex w-40 justify-between">
+           <ForYou/>
+           <DocsBtn/>
         </div>
     </div>
     </>
--- src/ui-components/HomeParts/SearchBar.jsx
+++ src/ui-components/HomeParts/SearchBar.jsx
@@ -2,12 +2,12 @@ import Searchicon from "./Searchicon";
 
 const SearchBar = () => {
   return (
-    <>
+    <div>
     <div className="w-2/3 mx-auto h-10 mt-8 rounded-md shadow-xl bg-white flex items-center px-3">
        <Searchicon/>
        <input className="w-full focus:outline-none pl-4" placeholder="Search your content on Canva's"/>
     </div>
-    </>
+    </div>
   )
 };
 export default SearchBar;
